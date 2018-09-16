ec43d607315b32231da77bb67a56a119edf28f07
Polina Shakhnova <shahnovapolina@gmail.com>
2018-09-16 20:33:49 +0300

    All functions without comparator

--- src/index.js
+++ src/index.js
@@ -1,31 +1,62 @@
 class Sorter {
   constructor() {
     // your implementation
+	 this.items = new Array(); 
   }
 
   add(element) {
-    // your implementation
+	  
+this.items.push(element);
+  //console.log('add' + element);
+
   }
 
   at(index) {
-    // your implementation
+	return this.items[index];
+	
   }
 
   get length() {
-    // your implementation
+	// console.log('length');
+    return this.items.length;
   }
 
   toArray() {
-    // your implementation
+
+  var arr = Array.from(this.items);
+	//console.log('toArray');
+    return arr;
   }
 
   sort(indices) {
-    // your implementation
+ indices.sort();
+	for (var i = 0, len = indices.length; i < len; ++i) {
+		var min = this.items[indices[i]];
+		
+		for ( var j = i+1, len = indices.length; j <= len; ++j) {
+			
+			if (this.items[indices[j]] < min)
+			{
+				var mm = this.items[indices[i]];
+				this.items[indices[i]]  = this.items[indices[j]];
+				this.items[indices[j]] = mm;
+			}
+		}
+	}
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
-  }
+	  //need to do!!!
+	  
+ //if (compareFunction>0) {
+//	 console.log('-1');
+  //  return -1;
+ // }
+ 
+ // return 1;
+ // 
+ }
 }
 
 module.exports = Sorter;
